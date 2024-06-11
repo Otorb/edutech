c764f8d79929c8247200aa336d794d56aed5e6d4
Otorb <otonielreyes0@gmail.com>
2024-06-11 18:40:40 -0400

    profiles ready

--- client/src/views/Prophile/ParentsAndSons.jsx
+++ client/src/views/Prophile/ParentsAndSons.jsx
@@ -1,103 +1,82 @@
-import React from 'react'
-import style from './style.module.css'
-import { Link } from 'react-router-dom'
-import { imagenHijo, imagenPadre, infoUser } from './ObjetoPrueba'
-import { useAppSelector } from '../../Hooks/useAppSelector'
+import React from "react";
+import style from "./style.module.css";
+import { Link } from "react-router-dom";
+import { imagenHijo, imagenPadre, infoUser } from "./ObjetoPrueba";
+import { useAppSelector } from "../../Hooks/useAppSelector";
 const ParentsAndSons = ({ estado }) => {
-    const state = estado
+  const student = estado;
+  console.log(student);
 
-    console.log(parent)
-    const student = estado
-    console.log(student)
-
-    return (
-        <>
-            <div className={style.bgContainer}>
-                <article className={style.articleRole}>
-                    <section className={style.infoUser}>
-                        <div className={style.ContainerUserInfo}>
-
-                            <div className={style.ToContactUser}>
-                                <div>
-                                    <b>Email  </b>
-                                    <a href='' onClick={(e) => e.preventDefault()}>{student?.email}</a>
-                                </div>
-                                <div>
-                                    <b>Celular  </b>
-                                    <a href='' onClick={(e) => e.preventDefault()}>{student?.phone}</a>
-                                </div>
-
-                                {parent?.role === "parent" && (
-                                    (<div>
-                                        <b>Direccion  </b>
-                                        <a href='' onClick={(e) => e.preventDefault()}>{student?.address || "no hay direccion"}</a>
-                                    </div>)
-                                )}
-                                {student?.rol === "student" &&
-                                    <div className={style.birthdayAndGrade}>
-                                        <b >F. de Nacimiento & Curso</b>
-                                        <div className={style.FechNavYGrado} >
-                                            <b >
-                                                {student?.birthd || null}
-                                            </b>
-                                            <b>
-                                                ({student?.grade || ""})
-                                            </b>
-                                        </div>
-                                    </div>}
-
-                            </div>
-                            {student?.rol === "student" && (
-                                <div className={style.subjectNotes}>
-                                    <div>
-                                        <b className={style.titleQualifications}>Calificaciones por materia</b>
-                                    </div>
-                                    {student?.Subjects.map(e => (
-                                        <div key={e?.idSubject} className={style.subjectAndCalification} >
-                                            <div className={style.ChildsubjectAndCalification}>
-                                                <b> Materia: ({e?.subjec})</b>
-                                            </div>
-                                            <div>
-
-                                                <b>Calificacion en {e?.subjec} (vacío)</b>
-                                            </div>
-                                            --
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
-                            <div className={style.redireccionAPadre}>
-
-                                <Link to="/dashboard/profile">
-                                    <button>
-                                        Detalles
-                                    </button>
-                                </Link>
-                            </div>
-                        </div>
-                    </section>
-                    <div className={style.containerPicture}>
-                        <section>
-
-                            <div>
-                                <p className={style.Name}>{student?.fullName}</p>
-
-                            </div>
-                            <div>
-                                {
-                                    estado?.rol === "student" ?
-                                        <p className={style.RolTitle} >
-                                            Alumno
-                                        </p> : null
-                                }
-
-                            </div>
-                        </section>
-                        <img className={style.imgRole} src={imagenHijo} alt="" />
+  return (
+    <>
+      <div className={style.bgContainer}>
+        <article className={style.articleRole}>
+          <section className={style.infoUser}>
+            <div className={style.ContainerUserInfo}>
+              <h1 className={style.titlePpal}>¡Bienvenido a EduSync!</h1>
+              <div className={style.ToContactUser}>
+                <div>
+                  <b className={style.detailInfo}>Correo: </b>
+                  {student?.email}
+                </div>
+                <div>
+                  {student.role === "parent" ? (
+                    <div>
+                      <b className={style.detailInfo}>Direccion </b>{" "}
+                      {student.address}
                     </div>
-                </article>
+                  ) : (
+                    <div>
+                      <b className={style.detailInfo}>Grado </b>
+                      {student?.Curso?.curso}
+                    </div>
+                  )}
+                </div>
+                <div>
+                <b className={style.detailInfo}>Celular </b>
+                  {student?.phone}
+                </div>
+              </div>
+              <div className={style.redireccionAPadre}>
+                {student.role === "student" ? (
+                  <Link to="/dashboard/profile">
+                    <button>Detalles</button>
+                  </Link>
+                ) : (
+                  <div>
+                    <h2>Seleccione un Hijo</h2>
+                    {student.Students &&
+                      student.Students.map((e) => {
+                        return (
+                          <Link to={`/dashboard/${e.id}`}>
+                            <button className={style.btnSons}>
+                              {e.fullName}
+                            </button>
+                          </Link>
+                        );
+                      })}
+                  </div>
+                )}
+              </div>
             </div>
-        </>
-    )
-}
-export default ParentsAndSons
+          </section>
+          <div className={style.containerPicture}>
+            <section>
+              <div>
+                <p className={style.Name}>{student?.fullName}</p>
+              </div>
+            </section>
+
+            {student.role === "parent" ? (
+              <img className={style.imgRole} src={imagenPadre} alt="" />
+            ) : (
+              <img className={style.imgRole} src={imagenHijo} alt="" />
+            )}
+            <p>{student.role === "parent" ? "Padre" : "Estudiante"}</p>
+          </div>
+        </article>
+      </div>
+    </>
+  );
+};
+export default ParentsAndSons;
