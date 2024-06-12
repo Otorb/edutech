7a6018d1fbe9fc38d81c517c313a0e999fe64ef9
julio cesar <castillo19092001@gmail.com>
2024-06-12 12:37:38 -0500

    actualizsr photo

--- server/src/routes/parentsRouter.js
+++ server/src/routes/parentsRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-// import multer from "multer";
+import multer from "multer";
 import {
   newParentHandler,
   ChangePasswordHandler,
@@ -11,33 +11,23 @@ import {
 
 const parentsRouter = Router();
 
-// const upload = multer({
-//   storage: multer.memoryStorage(),
-// });
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
 
-// parentsRouter.post("/", upload.none(), newParentHandler);
+parentsRouter.post("/", upload.none(), newParentHandler);
 parentsRouter.put("/changePassword/", ChangePasswordHandler);
 parentsRouter.get("/search/:id", getParentHandler);
 parentsRouter.get("/searchAll/", getAllParentsHandler);
 
-// parentsRouter.put(
-//   "/updateWihtImage/:id",
-//   upload.array("image", 1),
-//   updateWithImageHandler
-// );
-// parentsRouter.put(
-//   "/updateWihtoutImage/:id",
-//   upload.none(),
-//   updateWithoutImageHandler
-// );
 parentsRouter.put(
   "/updateWihtImage/:id",
-
+  upload.array("image", 1),
   updateWithImageHandler
 );
 parentsRouter.put(
   "/updateWihtoutImage/:id",
-
+  upload.none(),
   updateWithoutImageHandler
 );
 
